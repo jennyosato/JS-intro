514a084b18bab894cef036b116b83bb0788f1862
Jennifer <st.jennyandy@gmail.com>
2022-06-22 02:08:30 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let myname = "Jennifer"
+let myheight = "5ft 2inch"
+let mycountry = "Nigeria"
+alert("My name is " + myname + "\n" + " I am " + myheight + " tall, and" + "\n" +" I am from " + mycountry)
